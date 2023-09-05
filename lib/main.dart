1a4974ef315b3871e10bafaa1e5d17e7acad8363
Archer <ogunkapc@gmail.com>
2023-09-05 14:07:39 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Amazon());
+}
+
+class Amazon extends StatelessWidget {
+  const Amazon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
